b3601aa234095d0714c9deb283e7e143b98e3ee1
Tomas Barros <aluno20782@ipt.pt>
2019-07-16 00:49:54 +0100

    Criação dos modelos.

---
+++ MaouHeroLanding/Models/Compras.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web;
+
+namespace MaouHeroLanding.Models
+{
+    public class Compras
+    {
+        public int id { get; set; }
+
+        public decimal preco { get; set; }
+
+        [ForeignKey("Encomendas")]
+        public int EncomendaFK { get; set; }
+        public Encomendas Encomenda { get; set; }
+
+        [ForeignKey("Artigo")]
+        public int ArtigoFK { get; set; }
+        public Artigos Artigo { get; set; }
+    }
+}
