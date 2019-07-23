5630ad4a6da1c194c9e3f8b30abad9b4ef339e60
Antonio Rodrigues <aluno20628@ipt.pt>
2019-07-23 21:52:53 +0100

    Removeção de alguns comentarios

--- MaouHeroLanding/Migrations/Configuration.cs
+++ MaouHeroLanding/Migrations/Configuration.cs
@@ -18,9 +18,9 @@ namespace MaouHeroLanding.Migrations
         protected override void Seed(MaouHeroLanding.Models.ApplicationDbContext context)
         {
             var artigos = new List<Artigos> {
-               new Artigos {ID=1, Nome="AnimeBodyPillow",Tipo="Daquimakura",Preco=9.99M,Data_Entrada=new DateTime(1999,6,10),imagem="AnimeBodyPillow.jpg",Descricao="almofada",Produtor="Bem fofa"},
-               new Artigos {ID=2, Nome="MyHeroAcademia",Tipo="BD",Preco=20M,Data_Entrada=new DateTime(1999,6,2),imagem="MyHeroAcademia.jpg",Descricao="BD",Produtor="banda desenhada"},
-               new Artigos {ID=3, Nome="MissKobayashisDragonMaid",Tipo="Daquimakura",Preco=5M,Data_Entrada=new DateTime(2010,9,9),imagem="MissKobayashisDragonMaid.jpg",Descricao="lewd",Produtor="Nope"}
+               new Artigos {ID=1, Nome="Anime Body Pillow",Tipo="Dakimakura",Preco=9.99M,Data_Entrada=new DateTime(1999,6,10),imagem="AnimeBodyPillow.jpg",Descricao="almofada",Produtor="Bem fofa"},
+               new Artigos {ID=2, Nome="My Hero Academia",Tipo="Mangá",Preco=20M,Data_Entrada=new DateTime(1999,6,2),imagem="MyHeroAcademia.jpg",Descricao="BD",Produtor="banda desenhada"},
+               new Artigos {ID=3, Nome="Miss Kobayashis Dragon Maid",Tipo="Light Novel",Preco=5M,Data_Entrada=new DateTime(2010,9,9),imagem="MissKobayashisDragonMaid.jpg",Descricao="lewd",Produtor="Nope"}
             };
             artigos.ForEach(aa => context.Artigos.AddOrUpdate(a => a.Nome, aa));
             context.SaveChanges();
